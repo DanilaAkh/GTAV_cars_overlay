690b365341fdcc44986072d5d50a567de9507a50
Impaler67 <dockchoseive@gmail.com>
2026-07-22 20:27:09 +0500

    getting a car name, comparing it to one in the database & showing the car image ------- it mustn't work but it does o_O

--- show_img.py
+++ show_img.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 from io import BytesIO
 
 conn = sqlite3.connect('gtav_cars.db')
-c = conn.cursor()
+
 
 root = Tk()
 root.title("SQLite Image Viewer")
@@ -15,12 +15,15 @@ def show(data):
     Label(root,image=img).pack()
     root.image = img # Keep a reference
 
-def fetch():
-    c = conn.cursor()
-    id = 1 # Any id
-    c.execute('SELECT photo FROM cars where id=?',(id,))
-    data = c.fetchall()[0][0] # Get the blob data
+def fetch(car):
+    with conn:
+        c = conn.cursor()
+        name = car # Any id
+        c.execute('SELECT photo FROM cars where name=?',(name,))
+        data = c.fetchall()[0][0] # Get the blob data
     show(data) # Call the function with the passes data
+    
 
-fetch()
-root.mainloop()
+def show_img(car):
+    fetch(car)
+    root.mainloop()
